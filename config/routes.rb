a9e29ea6c7484fc15b9cc8a6b6e2900a32089cde
wuyepabdul <wuyepabdul@gmail.com>
2022-02-08 14:03:41 +0100

    Create migration for foods table and create foods controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/create'
+  get 'foods/delete'
   devise_for :users
   root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
