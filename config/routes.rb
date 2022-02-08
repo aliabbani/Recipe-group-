136d5e567d378b93708ee963a5f492a9d813da87
ali abbani <aliox@ahlam.com>
2022-02-08 11:51:07 +0200

    Add device gem, and make sure that a user can sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :users 
 
   # Defines the root path route ("/")
   # root "articles#index"
