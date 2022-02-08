ac38236c882ea7e02b0367092889b5ce06834d47
ali abbani <aliox@ahlam.com>
2022-02-08 12:46:02 +0200

    Add validation to user model, add bootstrap , add a navigation menu that allow user to sign in and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+    validates :name, :email, :password, presence: true
 end
