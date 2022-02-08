d74d34437626f7bc7f40d6b5c8eedee007515953
wuyepabdul <wuyepabdul@gmail.com>
2022-02-08 15:14:37 +0100

    Add form to create food in new.html.erb

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,12 +7,16 @@ class FoodsController < ApplicationController
     @food = Food.find(params[:id])
   end
 
+  def new
+    @food = Food.new
+  end
+
   def create
     food = current_user.foods.new(food_params)
     if food.save
       redirect_to foods_path, notice: 'Food created successfully'
     else
-      flash.now[:alert] = 'Failed to create food'
+      flash.now[:alert] = food.errors
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_125847) do
+ActiveRecord::Schema.define(version: 2022_02_08_140759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
-    t.integer "measurement_unit"
+    t.string "measurement_unit"
     t.integer "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
