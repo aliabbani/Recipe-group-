cead2895d043d0abefcefe7df8ff52da7187c6db
wuyepabdul <wuyepabdul@gmail.com>
2022-02-08 14:42:14 +0100

    Add index, show and create food actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,25 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all
   end
 
   def show
+    @food = Food.find(params[:id])
   end
 
   def create
+    food = current_user.foods.new(food_params)
+    if food.save
+      redirect_to foods_path, notice: 'Food created successfully'
+    else
+      flash.now[:alert] = 'Failed to create food'
+    end
   end
 
   def delete
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :price, :measurement_unit)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'foods/index'
-  get 'foods/show'
-  get 'foods/create'
-  get 'foods/delete'
   devise_for :users
-  root "users#index"
+  root "foods#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users 
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, except: [:edit, :update]
 end
