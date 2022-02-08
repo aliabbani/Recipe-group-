1918f4cb192832b04bee4ed248aad72079e99861
wuyepabdul <wuyepabdul@gmail.com>
2022-02-08 11:08:16 +0100

    Add letter opener gem to confirm emails

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,13 @@ ActiveRecord::Schema.define(version: 2022_02_08_093230) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
+    t.string "confirmation_token"
+    t.datetime "confirmed_at", precision: 6
+    t.datetime "confirmation_sent_at", precision: 6
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
