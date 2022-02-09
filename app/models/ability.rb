1d30f830dc5edac2d19806eb4a8dfd64c614919b
wuyepabdul <wuyepabdul@gmail.com>
2022-02-09 10:32:03 +0100

    Fix rubocop

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -8,6 +6,6 @@ class Ability
 
     return unless user.present?
 
-    can [:read, :create, :destroy], Food, user: user
+    can %i[read create destroy], Food, user: user
   end
 end
