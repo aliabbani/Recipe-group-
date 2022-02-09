39a78949344010123efb1bd0947960e9d5cf6f0c
wuyepabdul <wuyepabdul@gmail.com>
2022-02-09 10:53:52 +0100

    Create new schema for app

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_02_08_153212) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
