d29705931b2ab6a915d2c42c21b53debd1da3a2f
ali abbani <aliox@ahlam.com>
2022-02-11 21:50:00 +0200

    add spec test for features/public_recipe

---
+++ spec/features/public_recipes_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'Recipe #Show', type: :feature do
+  background do
+    visit new_user_session_path
+
+    @user = User.create(name: 'Ali', email: 'ali@mail.com', password: '123456', confirmed_at: Time.now)
+    @recipe = Recipe.create(name: 'burger', description: 'american burger', cooking_time: 10, preparation_time: 20,
+                            public: true, user_id: @user.id)
+
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+
+    click_button 'Log in'
+    visit public_recipes_path
+  end
+
+  scenario 'Display recipe details on page' do
+    expect(page).to have_content @recipe.name
+    expect(page).to have_content @user.name
+    expect(page).to have_content('Total food items:')
+    expect(page).to have_content('Total price:')
+  end
+
+  scenario 'show public status as true' do
+    expect(@recipe.public).to be_truthy
+  end
+
+  scenario 'Display recipe details on page' do
+    expect(page).to have_content 'Public Recipes'
+  end
+end
