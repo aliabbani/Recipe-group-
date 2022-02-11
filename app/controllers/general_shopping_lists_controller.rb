4b1b9fd97fac6d134cd591d801da6930dde2ff49
wuyepabdul <wuyepabdul@gmail.com>
2022-02-11 22:57:28 +0100

    Solve n+1 query and display total price

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,7 +1,16 @@
 class GeneralShoppingListsController < ApplicationController
+  def sum(array)
+    sum = 0
+    array.each do |number|
+      sum += number.food.price
+    end
+    sum
+  end
+
   def index
-    @recipes = Recipe.find(params[:recipe_id])
+    @recipe = Recipe.find(params[:recipe_id])
     @recipe_foods = RecipeFood.all
     @food = Food.all
+    @total_price = sum(@recipe.recipe_foods.includes([:food]))
   end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -4,4 +4,8 @@ class RecipeFood < ApplicationRecord
 
   validates :food_id, presence: true
   validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+  def total_price
+    recipe.recipe_foods.sum('price')
+  end
 end
